92ce2de33c9b4adecaf53c52b10c989a5e27c6ef
odevincenti <odevincenti@outlook.com>
2021-09-18 17:03:40 -0300

    Ejemplo para grafica impedancias

--- TC_TP2_ind.py
+++ TC_TP2_ind.py
@@ -6,7 +6,7 @@ from timespace import Timespace
 from calculate_H import draw_H, normalize_H
 
 def G(s, unit="rad/s"):
-    w0 = 2 * np.pi * f0
+    w0 = 2 * np.pi * fo
     if unit != "Hz":
         s = 2 * np.pi * s
     Gs = GI/(1 + 1j * s/w0)
@@ -19,7 +19,7 @@ def par(Ra, Rb):
     return Req
 
 R1 = 1E3
-R2 = 10E3
+R2 = 22E3
 R3 = 1E3
 R4 = 22E3
 Ao = 200E3
@@ -28,12 +28,12 @@ wb = 2*np.pi*fb
 interval = [1, 2E6, 1E4]
 
 GI = -R2/R1
-f0 = (Ao * fb)/(1 + R2 * (R1 + R3)/(R1 * R3))
+fo = (Ao * fb)/(1 + R2 * (R1 + R3)/(R1 * R3))
 
 # CALCULO GANANCIA
-w0 = 2 * np.pi * f0
+wo = 2 * np.pi * fo
 num = GI * np.array([1])
-den = np.array([1/w0, 1])
+den = np.array([1/wo, 1])
 
 # CALCULO IMPEDANCIA DE ENTRADA
 fn = Ao * fb / (R2/par(R1, R3))
@@ -42,6 +42,13 @@ fd = Ao * R3 * fb / R2
 wd = 2 * np.pi * fd
 numZi = [-R1/wn, -R1]
 denZi = [1/wd, 1]
+
+print("R2 = ", R2)
+print("fo = ", fo)
+print("fn ", fn)
+print("wn = ", wn)
+print("fd ", fd)
+print("wd = ", wd)
 
 #GRÁFICOS
 FS = Frecspace()        # Crea espacio de curvas de tiempo
@@ -69,14 +76,14 @@ FS.add_curve(1, [num, den, [1, 2E6, 1E5]], "Caso 3")
 
 # Si tarda es porque el Montecarlo tiene muchos puntos
 
-param = "G"
-FS.change_title("Ganancia del circuito inversor")
-# FS.change_title("Impedancia de entrada circuito inversor")
+param = "Zi"
+# FS.change_title("Ganancia del circuito inversor - Caso " + str(int(round((-1*(R2/1E3)**2 + 67 * R2/1E3 - 360)/210))))
+FS.change_title("Impedancia de entrada circuito inversor")
 
 FS.add_curve(4, "D:\Documentos\Materias\\22.01 - TC\TP2 Individual\Simulaciones\Inversor\Inv_mc" + str(int(round(R2/1E3))) + "k_" + param + ".txt", "Montecarlo", "silver", "Hz")
-FS.add_curve(1, [num, den, [1, 2E6, 1E5]], "Teórica", "blue")
+FS.add_curve(1, [numZi, denZi, [1, 2E6, 1E5]], "Teórica", "blue")
 FS.add_curve(2, "D:\Documentos\Materias\\22.01 - TC\TP2 Individual\Simulaciones\Inversor\Inv" + str(int(round(R2/1E3))) + "k_" + param + ".txt", "Simulación", "orange", "Hz")
-FS.add_curve(3, "D:\Documentos\Materias\\22.01 - TC\TP2 Individual\Mediciones\Inv" + str(int(round(R2/1E3))) + "k_" + param + ".csv", "Medicion", "green", "Hz")
+FS.add_curve(3, "D:\Documentos\Materias\\22.01 - TC\TP2 Individual\Mediciones\Inv" + str(int(round(R2/1E3))) + "k_" + param + ".csv", "Medicion", "green", "Hz", "veces")
 # FS.change_mod_unit("veces")
 # FS.change_ph_unit("rad")
 #FS.change_y_mod_label("$Z_{inp}[Ohm]$")
